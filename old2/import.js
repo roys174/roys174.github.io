b48ce063905f74c677d37438f488e32c364011fa
Roy Schwartz <roysch@164.7.208.128.dynr.cs.washington.edu>
2020-05-13 15:28:09 -0700

    new website

---
+++ old2/import.js
@@ -0,0 +1,209 @@
+var ARI = PERSON("http://www.cs.huji.ac.il/~arir/", "Ari Rappoport");
+var ROI = PERSON("http://ie.technion.ac.il/~roiri/", "Roi Reichart");
+var OMRI = PERSON("http://www.cs.huji.ac.il/~oabend/", "Omri Abend");
+var HOME = "https://roys174.github.io/";
+//var HOME = "http://www.cs.huji.ac.il/~roys02/";
+var NOAH = PERSON("http://homes.cs.washington.edu/~nasmith/", "Noah A. Smith");
+var HAO = PERSON("http://homes.cs.washington.edu/~hapeng/", "Hao Peng");
+var ROWAN = PERSON("http://rowanzellers.com/", "Rowan Zellers");
+var YONATAN = PERSON("http://yonatanbisk.com/", "Yonatan Bisk");
+var SAM = PERSON("http://samthomson.com/", "Sam Thomson");
+var MAARTEN = PERSON("http://homes.cs.washington.edu/~msap/", "Maarten Sap");
+var YEJIN = PERSON("http://homes.cs.washington.edu/~yejin/", "Yejin Choi");
+var YANNIS = PERSON("http://www.ikonstas.net/", "Yannis Konstas");
+var LI = "Li Zilles";
+var SUCHIN= PERSON("https://suchin.io/", "Suchin Gururangan");
+var DALLAS= PERSON("http://web.stanford.edu/~dcard/", "Dallas Card");
+var MATTP= PERSON("https://scholar.google.com/citations?user=K5nCPZwAAAAJ&hl", "Matthew E. Peters");
+var MARKN= PERSON("http://markneumann.xyz", "Mark Neumann");
+var ROB= PERSON("https://rloganiv.github.io", "Robert Logan");
+var VIDUR= PERSON("https://scholar.google.com/citations?user=NobF_hEAAAAJ&hl", "Vidur Joshi");
+var SAMEER= PERSON("http://sameersingh.org", "Sameer Singh");
+var SWABHA = PERSON("http://www.cs.cmu.edu/~sswayamd/", "Swabha Swayamdipta");
+var JESSE = PERSON("http://www.cs.cmu.edu/~jessed/", "Jesse Dodge");
+var OMER = PERSON("https://levyomer.wordpress.com/", "Omer Levy");
+var NELSON = PERSON("https://cs.stanford.edu/~nfliu/", "Nelson F. Liu");
+var GABRIELH = PERSON("http://gabrielilharco.com", "Gabriel Ilharco");
+var ALI = PERSON("https://homes.cs.washington.edu/~ali/", "Ali Farhadi");
+var HANNANEH = PERSON("https://homes.cs.washington.edu/~hannaneh/", "Hannaneh Hajishirzi");
+var CHENHAO = PERSON("https://chenhaot.com/", "Chenhao Tan");
+var SAMB = PERSON("https://www.nyu.edu/projects/bowman/", "Sam Bowman");
+var DONGYEOP = PERSON("http://www.cs.cmu.edu/~dongyeok/", "Dongyeop Kang");
+var ED = PERSON("http://www.cs.cmu.edu/~hovy/", "Eduard Hovy");
+var WALEED = PERSON("http://allenai.org/team/waleeda/", "Waleed Ammar");
+var BHAVANA = PERSON("http://allenai.org/team/bhavanad/", "Bhavana Dalvi");
+var ORENE = PERSON("http://allenai.org/team/orene/", "Oren Etzioni");
+var ORENT = PERSON("http://www.ise.bgu.ac.il/OrenTsur/", "Oren Tsur");
+var KOPPEL = PERSON("http://u.cs.biu.ac.il/~koppel/", "Moshe Koppel");
+var IVAN = PERSON("https://sites.google.com/site/ivanvulic/", "Ivan Vulić");
+var ANNAK = PERSON("http://www.cl.cam.ac.uk/~alk23/", "Anna Korhonen");
+var GABI = PERSON("https://gabrielstanovsky.github.io", "Gabi Stanovsky");
+var GAIL = PERSON("https://sgailw.cswp.cs.technion.ac.il", "Gail Weiss");
+var YOAV = PERSON("https://u.cs.biu.ac.il/~yogo/", "Yoav Goldberg");
+var ERANY = PERSON("http://www.cs.technion.ac.il/~yahave/", "Eran Yahav");
+var WILL = PERSON("https://lambdaviking.com", "Will Merrill");
+var ME = "<span><b>Roy Schwartz</b></span>";
+var ME_LINK = "<a href=" + HOME + " target=\"_blank\">Roy Schwartz</a>";
+
+// var colors = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71", "#006600", "#00CCCC", "#CC0066", "#FB8B24", "#8CADA7"]
+var tag_color = "white";
+var colors = [tag_color, tag_color,tag_color,tag_color,tag_color,tag_color,tag_color,tag_color,tag_color,tag_color]
+
+function PERSON(x,y) {
+	return ('<a href="'+x+'" target="_blank"><span class="people_link">'+y+'</span></a>');
+}
+
+function YEAR(y) {
+    if (y > 5) {
+        document.write("<h3><font color='black'></font>" + y + "</h3>");
+    }
+}
+
+function PDF(x) {
+	FIELD(x, "pdf")
+}
+
+function ABSTRACT(x) {
+	FIELD('#', 'abstract', '&nbsp&nbsp', 'onclick="$('+ "'"+x+"'"+').toggle();return false;"');
+	// document.write(' <a href="#" onclick="$('+ "'"+x+"'"+').toggle();return false;"><span>abstract</span></a> ');
+}
+
+function BIB(x) {
+	FIELD(x, "bib");
+}
+
+function SLIDES(x) {
+	FIELD(x, "slides");
+}
+
+function PRESS(x, y) {
+	FIELD(y, x, '');
+}
+
+function POSTER(x) {
+	FIELD(x, "poster");
+}
+
+function VIDEO(x) {
+	FIELD(x, "video");
+}
+
+function INTERVIEW(x) {
+	FIELD(x, "podcast interview");
+}
+
+
+function CODE(x) {
+	FIELD(x, "code");
+}
+
+function DATA(x) {
+	FIELD(x, "data");
+}
+
+function WEBSITE(x) {
+	FIELD(x, "website");
+}
+
+
+function GREENAI(filter) {
+	HASHTAG("greenai", 6, filter);
+}
+
+function RATIONAL_RECURRENCES(filter) {
+	HASHTAG("understanding_models", 7, filter);
+}
+
+function ARTIFACTS(filter) {
+	HASHTAG("improved_evaluation", 8, filter);
+}
+
+function EMBEDDINGS(filter) {
+	HASHTAG("word_representation", 9, filter);
+}
+
+
+function FIELD(x, y, suffix="&nbsp&nbsp", extra_args="target='_blank'") {
+	document.write("<a href="+ x +' ' + extra_args+" class='internal_link'><span class='internal_link'>"+y+"</span></a>"+suffix);
+	// document.write('<a href="#"><span>abstract</span></a> ');
+
+}
+
+function HASHTAG(x, bgcolor_index, filter=false) {
+	prefix = '';
+	suffix = '';
+	if (filter) {
+		prefix = '<a onclick="filter_list('+"'"+x+"'"+');">';
+		suffix = '</a>';
+		var list = document.getElementsByClassName("papers");
+	
+		for (i = 0; i < list.length; i++) {
+			document.write("paper is "+list[i].id);
+		  	// list[i].style.display = "none";
+		}
+	}
+	
+	document.write("&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+prefix+"<span style='color:gray;background-color:" + colors[bgcolor_index] +";'><i> #"+x+" </i></span>"+suffix+" ");	
+}
+
+function filter_list(name) {
+	var list = document.getElementsByClassName("all");
+	// document.write(name);
+	
+	for (i = 0; i < list.length; i++) {
+		// document.write("paper is");
+		// document.write("paper is "+list[i]);
+	  	list[i].style.display = "none";
+	}
+	var list = document.getElementsByClassName(name);
+	// document.write("bobobo"+list.length+"\n");
+	
+	for (i = 0; i < list.length; i++) {
+		// document.write("paper is");
+		// document.write("paper is "+list[i]);
+	  	list[i].style.display = "block";
+	}
+		
+}
+
+function TITLE(x) {
+	document.write('<span><b>' + x + "</b></span>");
+}
+
+function TITLEF(x, y) {
+	document.write("<a href='" + x +"'"+'><span><b>' + y + "</b></span></a>");
+}
+
+
+function URL(x, y, z) {
+	var extra = '';
+	if (arguments.length > 3) {
+		extra = arguments[3];
+	}
+	document.write("<a href='" + x + y + "' " + extra + ">" + z + "</a>");
+}
+
+// function email() {
+// 	document.write('r o y s <img border=0 width=35 height=auto src='+HOME+'index_files/strudel.jpg> allenai . org ');
+// 	document.write('&nbsp&nbsp&nbsp&nbsp&nbsp<a href="https://twitter.com/royschwartz02" target="_blank"><img src="index_files/twitter.png" style="margin-bottom:0px;height:30px;border:0px;display:inline"> @royschwartz2 </a>');
+// }
+
+
+function CONFERENCE(url, name, award=undefined, prefix="In proceedings of ") {
+	var suffix = '';
+	if (typeof award !== 'undefined') {
+		suffix = '</i><span style="color:red"> <b>[<i>'+award+'</i>]</span></b>';
+	}
+		
+	document.write(prefix+'<i><a href="'+url+'" target="_blank"><span class="people_link">'+name+'</span></a></i>'+suffix+'<br>');
+}
+
+
+function TOP_PART(title='') {
+	HEADER();
+	document.write('<div class="top"><div class="top_style"><div style="top: 100px; align:top-left;position: absolute;max-width:100%" class="page_titles">Schwartz Lab<br><h1 class="h1_style" style="max-width:90%">'+title+'</h1></div><img class="fluidimage" align="right" SRC="index_files/roy_schwartz.jpg" style="max-height:100%;max-width:25%;"/></div></div>');	
+}
+
+function HEADER() {
+	document.write('<div><ul class="menu"><li class="menu"><a href="./index.html">Home</a></li> <li class="menu"><a href="./people.html">People</a></li> <li class="menu"><a href="publications.html">Publications</a></li><li class="menu"><a href="talks.html">Talks</a></li> <li class="menu"><a href="joinus.html">Join Us!</a></li></ul> </div>')
+}
---
+++ old2/old/import.js
@@ -0,0 +1,189 @@
+var ARI = "<a href=\"http://www.cs.huji.ac.il/~arir/\" target=\"_blank\">Ari Rappoport</a>";
+var ROI = "<a href=\"http://ie.technion.ac.il/~roiri/\" target=\"_blank\">Roi Reichart</a>";
+var OMRI = "<a href=\"http://www.cs.huji.ac.il/~oabend/\" target=\"_blank\">Omri Abend</a>";
+var HOME = "https://roys174.github.io/";
+//var HOME = "http://www.cs.huji.ac.il/~roys02/";
+var NOAH = "<a href=\"http://homes.cs.washington.edu/~nasmith/\" target=\"_blank\">Noah A. Smith</a>";
+var HAO = "<a href=\"http://homes.cs.washington.edu/~hapeng/\" target=\"_blank\">Hao Peng</a>";
+var ROWAN = "<a href=\"http://rowanzellers.com/\" target=\"_blank\">Rowan Zellers</a>";
+var YONATAN = "<a href=\"http://yonatanbisk.com/\" target=\"_blank\">Yonatan Bisk</a>";
+var SAM = "<a href=\"http://samthomson.com/\" target=\"_blank\">Sam Thomson</a>";
+var MAARTEN = "<a href=\"http://homes.cs.washington.edu/~msap/\" target=\"_blank\">Maarten Sap</a>";
+var YEJIN = "<a href=\"http://homes.cs.washington.edu/~yejin/\" target=\"_blank\">Yejin Choi</a>";
+var YANNIS = "<a href=\"http://www.ikonstas.net/\" target=\"_blank\">Yannis Konstas</a>";
+var LI = "Li Zilles";
+var SUCHIN= "<a href=\"https://suchin.io/\" target=\"_blank\">Suchin Gururangan</a>";
+var DALLAS= "<a href=\"http://web.stanford.edu/~dcard/\" target=\"_blank\">Dallas Card</a>";
+var MATTP= "<a href=\"https://scholar.google.com/citations?user=K5nCPZwAAAAJ&hl\" target=\"_blank\">Matthew E. Peters</a>";
+var MARKN= "<a href=\"http://markneumann.xyz\" target=\"_blank\">Mark Neumann</a>";
+var ROB= "<a href=\"https://rloganiv.github.io\" target=\"_blank\">Robert Logan</a>";
+var VIDUR= "<a href=\"https://scholar.google.com/citations?user=NobF_hEAAAAJ&hl\" target=\"_blank\">Vidur Joshi</a>";
+var SAMEER= "<a href=\"http://sameersingh.org\" target=\"_blank\">Sameer Singh</a>";
+var SWABHA = "<a href=\"http://www.cs.cmu.edu/~sswayamd/\" target=\"_blank\">Swabha Swayamdipta</a>";
+var JESSE = "<a href=\"http://www.cs.cmu.edu/~jessed/\" target=\"_blank\">Jesse Dodge</a>";
+var OMER = "<a href=\"https://levyomer.wordpress.com/\" target=\"_blank\">Omer Levy</a>";
+var NELSON = "<a href=\"https://cs.stanford.edu/~nfliu/\" target=\"_blank\">Nelson F. Liu</a>";
+var GABRIELH = "<a href=\"http://gabrielilharco.com\" target=\"_blank\">Gabriel Ilharco</a>";
+var GABI = "<a href=\"https://gabrielstanovsky.github.io\" target=\"_blank\">Gabi Stanovsky</a>";
+var GAIL = "<a href=\"https://sgailw.cswp.cs.technion.ac.il\" target=\"_blank\">Gail Weiss</a>";
+var YOAV = "<a href=\"https://u.cs.biu.ac.il/~yogo/\" target=\"_blank\">Yoav Goldberg</a>";
+var ERANY = "<a href=\"http://www.cs.technion.ac.il/~yahave/\" target=\"_blank\">Eran Yahav</a>";
+var WILL = "<a href=\"https://lambdaviking.com\" target=\"_blank\">Will Merrill</a>";
+var ALI = "<a href=\"https://homes.cs.washington.edu/~ali/\" target=\"_blank\">Ali Farhadi</a>";
+var HANNANEH = "<a href=\"https://homes.cs.washington.edu/~hannaneh/\" target=\"_blank\">Hannaneh Hajishirzi</a>";
+var CHENHAO = "<a href=\"https://chenhaot.com/\" target=\"_blank\">Chenhao Tan</a>";
+var SAMB = "<a href=\"https://www.nyu.edu/projects/bowman/\" target=\"_blank\">Sam Bowman</a>";
+var DONGYEOP = "<a href=\"http://www.cs.cmu.edu/~dongyeok/\" target=\"_blank\">Dongyeop Kang</a>";
+var ED = "<a href=\"http://www.cs.cmu.edu/~hovy/\" target=\"_blank\">Eduard Hovy</a>";
+var WALEED = "<a href=\"http://allenai.org/team/waleeda/\" target=\"_blank\">Waleed Ammar</a>";
+var BHAVANA = "<a href=\"http://allenai.org/team/bhavanad/\" target=\"_blank\">Bhavana Dalvi</a>";
+var ORENE = "<a href=\"http://allenai.org/team/orene/\" target=\"_blank\">Oren Etzioni</a>";
+var ME = "<span style='color:maroon'><b>Roy Schwartz</b></span>";
+var ME_LINK = "<a href=" + HOME + " target=\"_blank\">Roy Schwartz</a>";
+
+var colors = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71", "#006600", "#00CCCC", "#CC0066", "#FB8B24", "#8CADA7"]
+
+function YEAR(y) {
+    if (y > 5) {
+        document.write("<h3><font color='black'></font>" + y + "</h3>");
+    }
+}
+
+function PDF(x) {
+	FIELD(x, "PDF", 0)
+}
+
+function ABSTRACT(x) {
+	document.write('<a href="#" onclick="$('+ "'"+x+"'"+').toggle();return false;"><span style="background-color:'+colors[0]+';color:white;white-space:pre-wrap;font-family:monospace;"> ABSTRACT </span></a>');
+}
+
+function BIB(x) {
+	FIELD(x, "BIB", 0);
+}
+
+function SLIDES(x) {
+	FIELD(x, "SLIDES", 0);
+}
+
+function PRESS(x, y) {
+	FIELD(y, x, 5);
+}
+
+function POSTER(x) {
+	FIELD(x, "POSTER", 0);
+}
+
+function VIDEO(x) {
+	FIELD(x, "VIDEO", 0);
+}
+
+function INTERVIEW(x) {
+	FIELD(x, "PODCAST INTERVIEW", 5);
+}
+
+
+function CODE(x) {
+	FIELD(x, "CODE", 0);
+}
+
+function DATA(x) {
+	FIELD(x, "DATA", 0);
+}
+
+function WEBSITE(x) {
+	FIELD(x, "WEBSITE", 0);
+}
+
+
+function GREENAI(filter) {
+	HASHTAG("greenai", 6, filter);
+}
+
+function RATIONAL_RECURRENCES(filter) {
+	HASHTAG("understanding_nlp_models", 7, filter);
+}
+
+function ARTIFACTS(filter) {
+	HASHTAG("improving_evaluation", 8, filter);
+}
+
+function EMBEDDINGS(filter) {
+	HASHTAG("word_representation", 9, filter);
+}
+
+
+function FIELD(x, y, bgcolor_index) {
+	document.write("<a href='"+ x +"' target='_blank'><span style='background-color:" + colors[bgcolor_index] +";color:white;white-space:pre-wrap;font-family:monospace;'> "+ y +" </span></a>");
+}
+
+function HASHTAG(x, bgcolor_index, filter=false) {
+	prefix = '';
+	suffix = '';
+	if (filter) {
+		prefix = '<a onclick="filter_list('+"'"+x+"'"+');">';
+		suffix = '</a>';
+		var list = document.getElementsByClassName("papers");
+	
+		for (i = 0; i < list.length; i++) {
+			document.write("paper is "+list[i].id);
+		  	// list[i].style.display = "none";
+		}
+	}
+	
+	document.write(" "+prefix+"<span style='background-color:" + colors[bgcolor_index] +";color:white;white-space:pre-wrap;font-family:monospace;'> #"+x+" </span>"+suffix+" ");	
+}
+
+function filter_list(name) {
+	var list = document.getElementsByClassName("all");
+	// document.write(name);
+	
+	for (i = 0; i < list.length; i++) {
+		// document.write("paper is");
+		// document.write("paper is "+list[i]);
+	  	list[i].style.display = "none";
+	}
+	var list = document.getElementsByClassName(name);
+	// document.write("bobobo"+list.length+"\n");
+	
+	for (i = 0; i < list.length; i++) {
+		// document.write("paper is");
+		// document.write("paper is "+list[i]);
+	  	list[i].style.display = "block";
+	}
+		
+}
+
+function TITLE(x) {
+	document.write('<span style="color:#3366cc"><b>' + x + "</b></span>");
+}
+
+function TITLEF(x, y) {
+	document.write("<a href='" + x +"'"+'><span style="color:#3366cc"><b>' + y + "</b></span></a>");
+}
+
+
+function URL(x, y, z) {
+	var extra = '';
+	if (arguments.length > 3) {
+		extra = arguments[3];
+	}
+	document.write("<a href='" + x + y + "' " + extra + ">" + z + "</a>");
+}
+
+function email() {
+    document.write('r o y s<img border=0 width=66 height=52 src='+HOME+'index_files/strudel.jpg> allenai . org</p>');
+}
+
+function CONFERENCE(url, name, award=undefined) {
+	var suffix = '';
+	if (typeof award !== 'undefined') {
+		suffix = '</i><span style="color:red"> <b>[<i>'+award+'</i>]</span></b>';
+	}
+		
+	document.write('<i>In proceedings of <a href="'+url+'" target="_blank"><span style="color:CornflowerBlue">'+name+'</span></a></i>'+suffix+'<br>');
+}
+
+
+function TOP_PART() {
+document.write('<div class="split top" style="margin-left: 30px"><hr><div style="height: 180px"><div style="top: 100px; position: absolute;" class=page_titles>Roy Schwartz</div><img class="fluidimage" align="right" SRC="index_files/roy_schwartz.jpg" style="max-height:100%;max-width:100%;"/></div>');	
+}
